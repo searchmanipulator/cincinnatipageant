1e029ba32404e87885c9caf14446e7d7baa26a9c
Jon Namnath <jnamnath@gmail.com>
2012-08-07 11:03:35 -0700

    Add a php.ini file. JN

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 
-php_info(); exit;
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
